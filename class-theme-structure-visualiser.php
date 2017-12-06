395a866f922a8e8ea546eda4cb7bdfac0cad563d
Shantanu <shantanu2846@gmail.com>
2017-12-06 16:44:51 +0530

    Create class file class-theme-structure-visualiser.php

---
+++ class-theme-structure-visualiser.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Theme structure visualiser
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+
+if ( !defined( ABSPATH ) ) {
+	exit();
+}
+
+if ( !class_exists( 'Theme_Structure_Visualiser' ) ) {
+
+	/**
+	 * Theme structure visualiser
+	 *
+	 * @since 0.0.1
+	 */
+	class Theme_Structure_Visualiser {
+		
+	}
+
+}
