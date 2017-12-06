683321ca8a174592ce3d1912ff018246886e6522
Shantanu <shantanu2846@gmail.com>
2017-12-06 16:32:05 +0530

    Add plugin header to the main plugin file

--- theme-structure-visualiser.php
+++ theme-structure-visualiser.php
@@ -1,1 +1,13 @@
 <?php
+
+/*
+Plugin Name: Theme Structure Visualiser
+Plugin URI	  :  
+Description  : Visualise the basic structure of a website
+Version	  : 0.0.1
+Author	  : Shantanu Desai
+Author URI  : https://github.com/shantanu2704
+Text Domain: theme-structure-visualiser
+Domain Path: /languages
+License	  : GPL2
+ */
