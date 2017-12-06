171357859afbfa968a73b11bdb91d0561fc5c79f
Shantanu <shantanu2846@gmail.com>
2017-12-06 17:41:25 +0530

    Add the init function in the core plugin class

--- includes/class-theme-structure-visualiser.php
+++ includes/class-theme-structure-visualiser.php
@@ -6,7 +6,6 @@
  * @author Shantanu Desai <shantanu2846@gmail.com>
  * @since 0.0.1
  */
-
 // If this file is called directly, abort.
 if ( !defined( ABSPATH ) ) {
 	exit();
@@ -20,7 +19,19 @@ if ( !class_exists( 'Theme_Structure_Visualiser' ) ) {
 	 * @since 0.0.1
 	 */
 	class Theme_Structure_Visualiser {
-		
+
+		/**
+		 * Initialise the class
+		 *
+		 * Description.
+		 *
+		 * @since 0.0.1 
+		 */
+		function init() {
+			add_action( 'all', array( $this, 'get_included_file_names' ) );
+			add_action( 'all', array( $this, 'print_header' ) );
+		}
+
 	}
 
 }
