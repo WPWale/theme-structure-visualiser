b67b32d9939af747e95f9283c7d1476562f8da12
Shantanu <shantanu2846@gmail.com>
2017-12-07 12:17:48 +0530

    Add get_included_files() to the core plugin class

--- includes/class-theme-structure-visualiser.php
+++ includes/class-theme-structure-visualiser.php
@@ -19,7 +19,7 @@ if ( !class_exists( 'Theme_Structure_Visualiser' ) ) {
 	 * @since 0.0.1
 	 */
 	class Theme_Structure_Visualiser {
-
+		
 		/**
 		 * Initialise the class
 		 *
@@ -29,7 +29,51 @@ if ( !class_exists( 'Theme_Structure_Visualiser' ) ) {
 		 */
 		function init() {
 			add_action( 'all', array( $this, 'get_included_file_names' ) );
-			add_action( 'all', array( $this, 'print_header' ) );
+			add_action( 'wp_head', array( $this, 'print_header' ) );
+		}
+
+		/**
+		 * Initialise the class
+		 *
+		 * Description.
+		 *
+		 * @since 0.0.1 
+		 */
+		function get_included_file_names() {
+			$mm_current_filter = current_filter();
+
+			$patterns = array();
+
+			foreach ( $file_types as $type ) {
+				$patterns[ $type ] = "get_$type";
+			}
+
+			if ( !in_array( $mm_current_filter, $patterns ) ) {
+				if ( !strstr( $mm_current_filter, $patterns[ 'template_part' ] ) ) {
+					return;
+				}
+			}
+			$hook_arguments = func_get_args();
+			$slug = $name = '';
+
+			if ( in_array( $mm_current_filter, $patterns ) ) {
+				$patterns_flip = array_flip( $patterns );
+				$slug = $patterns_flip[ $mm_current_filter ];
+				$name = $hook_arguments[ 1 ];
+			}
+
+			if ( strstr( $mm_current_filter, $patterns[ 'template_part' ] ) ) {
+				$slug	 = $hook_arguments[ 1 ];
+				$name = $hook_arguments[ 2 ];
+			}
+
+			if ( 'header' !== $slug ) {
+				mm_print_path( $slug, $name );
+			} else {
+				global $mm_header_slug, $mm_header_name;
+				$mm_header_slug	 = $slug;
+				$mm_header_name	 = $name;
+			}
 		}
 
 	}
