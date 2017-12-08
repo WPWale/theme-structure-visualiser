5c9bd8a47c4d3b958b83f46eb45fbc9b68b7f69d
Shantanu <shantanu2846@gmail.com>
2017-12-08 09:10:30 +0530

    Fix plugin activation problem

--- includes/class-theme-structure-visualiser.php
+++ includes/class-theme-structure-visualiser.php
@@ -7,7 +7,7 @@
  */
 
 // If this file is called directly, abort.
-if ( !defined( ABSPATH ) ) exit();
+if ( !defined( 'ABSPATH' ) ) exit();
 
 if ( !class_exists( 'Theme_Structure_Visualiser' ) ) {
 
--- theme-structure-visualiser.php
+++ theme-structure-visualiser.php
@@ -10,18 +10,17 @@
  */
 /*
   Plugin Name: Theme Structure Visualiser
-  Plugin URI:	  :
-  Description  : Visualise the basic structure of a website
-  Version	  : 0.0.1
-  Author	  : Shantanu Desai
-  Author URI  : https://github.com/shantanu2704
+  Description: Visualise the basic structure of a website
+  Version: 0.0.1
+  Author: Shantanu Desai
+  Author URI  https://github.com/shantanu2704
   Text Domain: theme-structure-visualiser
   Domain Path: /languages
-  License	  : GPL2
+  License: GPL2
  */
 
 // If this file is called directly, abort.
-if ( !defined( ABSPATH ) ) exit();
+if ( !defined( 'ABSPATH' ) ) exit();
 
 if ( !defined( 'TSV_PATH' ) ) {
 	/**
