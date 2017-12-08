de7eaffd01740def3ce66b45bdfd23c2d0f8e579
Shantanu <shantanu2846@gmail.com>
2017-12-08 16:44:42 +0530

    Refactor display-header-object script

--- assets/js/display-header-object.js
+++ assets/js/display-header-object.js
@@ -1,12 +1,16 @@
 jQuery('document').ready(function($){
     
-    // Create an HTML div with classes 'tsv-template-path ' and 'header'
-    var template = $('<div class="tsv-template-path header" />');
-    
-    // Print the path
-    template.html(tsv_header_filename.path);
-    
-    // Prepend the div to 'header.site-header'
-    $("header.site-header").prepend(template);
+    // Check if 'tsv_header_filename' is defined
+    if ( typeof tsv_header_filename != "undefined" ) {
+
+        // Create an HTML div with classes 'tsv-template-path ' and 'header'
+        var template = $( '<div class="tsv-template-path header" />' );
+
+        // Print the path
+        template.html( tsv_header_filename.path );
+
+        // Prepend the div to 'header.site-header'
+        $( "header.site-header" ).prepend( template );
+    }
     
 });
