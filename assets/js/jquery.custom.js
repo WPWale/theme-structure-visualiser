34b77942a80a8c76ebe40c33ffa6bb250754934c
Shantanu <shantanu2846@gmail.com>
2017-12-11 14:50:06 +0530

    Remove the  localization code from custom js

--- assets/js/jquery.custom.js
+++ assets/js/jquery.custom.js
@@ -1,9 +1,5 @@
 jQuery( 'document' ).ready( function ( $ ) {
     $( '.tsv-color-picker' ).wpColorPicker();
-    
-    if ( typeof tsv_custom_options != "undefined" ) {
-        (".tsv-template-path").css("background-color", tsv_custom_options.background_colour);
-        (".tsv-template-path").css("color", tsv_custom_options.font_colour);
     }
-} );
+);
     
