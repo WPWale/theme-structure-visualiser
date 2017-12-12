b2aca8a0cdad321c2240e9bb79361f41b2881ab3
Shantanu <shantanu2846@gmail.com>
2017-12-12 13:43:17 +0530

    close #11

--- templates/display-structure.php
+++ templates/display-structure.php
@@ -1,3 +1,15 @@
+<?php
+/**
+ * Display Structure
+ * 
+ * Output the path wrapped in a div.
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit(); ?>
+
 <div class="tsv-template-path <?php echo $class; ?>">
 	<?php echo $path; ?>
 </div>
