01d3a8fcee17919888f3ef1afad165e0389f6a69
Shantanu <shantanu2846@gmail.com>
2017-12-13 14:59:56 +0530

    Add prepend-to-next.js

---
+++ assets/js/prepend-to-next.js
@@ -0,0 +1,16 @@
+jQuery( 'document' ).ready( function ( $ ) {
+    $( '.tsv-template-path' ).each( function () {
+	var tpl_path_div = $( this );
+	if ( tpl_path_div.hasClass( 'footer' ) ) {
+	    $( "footer.site-footer" ).prepend( tpl_path_div );
+	} else if ( tpl_path_div.hasClass( 'sidebar' ) ) {
+	    $( "#secondary.widget-area" ).prepend( tpl_path_div );
+	} else {
+	    var next_elem = tpl_path_div.next();
+	    if ( !next_elem.hasClass( 'tsv-template-path' ) ) {
+		next_elem.prepend( tpl_path_div );
+	    }
+
+	}
+    } );
+} );
