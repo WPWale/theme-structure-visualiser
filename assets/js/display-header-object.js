45b4bb57e471cd060390589a5620e031997df498
Shantanu <shantanu2846@gmail.com>
2017-12-13 14:59:29 +0530

    Break display-header-object into two files

--- assets/js/display-header-object.js
+++ assets/js/display-header-object.js
@@ -14,18 +14,4 @@ jQuery( 'document' ).ready( function ( $ ) {
 
 	}
     } );
-
-    // Check if 'tsv_header_filename' is defined
-    if ( typeof tsv_header_filename != "undefined" ) {
-
-	// Create an HTML div with classes 'tsv-template-path ' and 'header'
-	var template = $( '<div class="tsv-template-path header" />' );
-
-	// Print the path
-	template.html( tsv_header_filename.path );
-
-	// Prepend the div to 'header.site-header'
-	$( "header.site-header" ).prepend( template );
-    }
-
 } );
